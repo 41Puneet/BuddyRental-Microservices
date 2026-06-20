ab566da4f223f0b86b7472aa4e1eb01bdd91d1e1
Puneet Agnihotri <puneetagnihotri1616@gmail.com>
2026-06-20 13:43:49 +0530

    added the booking repository

--- booking_service/src/main/java/com/booking_service/Repository/BookingRepository.java
+++ booking_service/src/main/java/com/booking_service/Repository/BookingRepository.java
@@ -2,8 +2,16 @@ package com.booking_service.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.booking_service.Entity.Booking;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking,UUID> {
+    Page<Booking>findByUserId(UUID userId,Pageable pageable);
+
+    Page<Booking>findByVehicleId(UUID vehicleId,Pageable pageable);
     
+    Optional<Booking>findByVehicleIdAndStartDate(UUID vehicleId, LocalDateTime startDate);
 
 }
