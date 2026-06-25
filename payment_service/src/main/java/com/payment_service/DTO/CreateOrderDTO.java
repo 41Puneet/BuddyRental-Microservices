9e3b6ec5eb95a64e50a683b7e9a3738bed912f8d
Puneet Agnihotri <puneetagnihotri1616@gmail.com>
2026-06-25 10:50:57 +0530

    fixed issues with the frontend

--- payment_service/src/main/java/com/payment_service/DTO/CreateOrderDTO.java
+++ payment_service/src/main/java/com/payment_service/DTO/CreateOrderDTO.java
@@ -26,5 +26,16 @@ public class CreateOrderDTO {
 /* 
 4771d141-cf6a-44fe-a24f-c9bea88ecce0 bookingId
 order_T5C8eonDUM6nhp orderId
+
+pay_T5CM5iMIl7bT2B transactionId
+
+"paymentId": "e67c6cd6-62c7-4418-b60d-7168c51887af",
+    "bookingId": "4771d141-cf6a-44fe-a24f-c9bea88ecce0",
+    "userId": "f422209e-243d-4612-92fd-a686b8317b55",
+    "transactionId": "pay_T5CM5iMIl7bT2B",
+    "amount": 1500.0,
+    "paymentStatus": "SUCCESS",
+    "createdAt": null,
+    "booking": null
 */
 
